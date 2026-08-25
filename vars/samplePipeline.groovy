15e8aa9c87360e351f4c02586dec3f6c9d69186b
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2026-08-25 12:12:50 +0530

    jenkins

--- vars/samplePipeline.groovy
+++ vars/samplePipeline.groovy
@@ -5,7 +5,6 @@ def call(Map configMap){
         }
         environment { 
             COURSE = 'jenkins'
-            greeting = configMap.get('greeting')
         }
         options {
             timeout(time: 30, unit: 'MINUTES') 
@@ -15,16 +14,17 @@ def call(Map configMap){
             string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
             text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
             booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+            choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+            password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
         }
+
         // Build
         stages {
             stage('Build') {
                 steps {
-                    script{
+                    script {
                         sh """
                             echo "Hello Build"
-                            sleep 10
-                            env
                             echo "Hello ${params.PERSON}"
                         """
                     }
@@ -32,12 +32,14 @@ def call(Map configMap){
             }
             stage('Test') {
                 steps {
-                    script{
-                        echo 'Testing..'
+                    script {
+                        sh """
+                            echo "Hello Test"
+                            env
+                        """
                     }
                 }
             }
-            
         }
 
         post { 
